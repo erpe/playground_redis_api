5945b3e2677d045cdb75cf8b973caeb7ec3e0926
rene paulokat <rene@so36.net>
2016-08-14 00:02:57 +0200

    Writes/Reads test from redis

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ extern crate rustc_serialize;
 extern crate redis;
 
 use nickel::Nickel;
+use redis::Commands;
+use std::io::{self, Write};
 
 #[derive(RustcDecodable, RustcEncodable)]
 
@@ -30,6 +32,8 @@ impl Organisation {
 fn main() {
     let mut server = Nickel::new();
 
+    set_redis_value_test();
+
     server.utilize(router! {
         get "/api" =>  |_req, _res| {
             "your api welcomes you!\n"
@@ -41,6 +45,7 @@ fn main() {
             let orga_id = req.param("orga_id").unwrap();
             if Organisation::is_available(orga_id.to_string()) {
                 let orga = Organisation::find();
+                get_redis_value_test();
                 format!("your'e asking for rating stats of: {}\nwe found: {}\n", orga_id, orga.name)
             } else {
                 format!("no organisation found: {}\n", orga_id)
@@ -55,4 +60,19 @@ fn find_organisation() -> Organisation {
                             shortname: "foo".to_string(), 
                             slug: "foo".to_string() };
     orga
+}
+
+// -> redis::RedisResult<()> 
+fn set_redis_value_test() {
+    let client = redis::Client::open("redis://127.0.0.1/11").unwrap();
+    let con = client.get_connection().unwrap();
+    let _ : () = con.set("ef", 43).unwrap();
+}
+
+fn get_redis_value_test() -> i32 {
+    let client = redis::Client::open("redis://127.0.0.1/11").unwrap();
+    let con = client.get_connection().unwrap();
+    let payload : i32 = con.get("ef").unwrap();
+    println!("payload: {}", payload);
+    payload
 }
